8d9fca8d82f41c730f36c432a63914fbfee8e4e4
Evgeniy-Muhamethin <John090918@gmail.com>
2023-05-01 23:33:05 +0300

    Data file was added

---
+++ RollAndBall/Assets/Scripts/Player/Data.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public struct Data 
+{
+    private float speed;
+
+    public float GetSpeed() => speed;
+    public void SetSpeed(float speed) => this.speed = speed;
+}
--- RollAndBall/Assets/Scripts/Player/MainPlayer.cs
+++ RollAndBall/Assets/Scripts/Player/MainPlayer.cs
@@ -8,8 +8,10 @@ public class MainPlayer : MonoBehaviour
 	private Transform _playerTransform;
 
 	private float _speedPlayer = 10f;
+	private float _dataSpeed;
 
 	private KeyCode[] _keyButton;
+	private Data _data;
 
 	private MovePlayer _movePlayer;
 
@@ -19,12 +21,15 @@ public class MainPlayer : MonoBehaviour
 		KeyCode.S, KeyCode.A, KeyCode.D };
 
 		_movePlayer = new MovePlayer();
+
+		_data.SetSpeed(_speedPlayer);
+		_dataSpeed = _data.GetSpeed();
 	}
 
 	private void FixedUpdate()
 	{
 		_movePlayer.Move(_playerTransform,
 			_keyButton[0], _keyButton[1],
-			_keyButton[2], _keyButton[3], _speedPlayer);
+			_keyButton[2], _keyButton[3], _dataSpeed);
 	}
 }
